a897bd8d9a1d4e4eb82173a2031cf61fbf213a7e
Glaucio Ricardo <glaucio.ricardo.jun@accenture.com>
2020-02-05 00:03:25 -0300

    estado feito com sucesso

--- src/components/testae.js
+++ src/components/testae.js
@@ -1,18 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { recuperarPersonagensRequest } from '../redux/Characters/Retrieve/actions';
 
 const Testae = () => {
   const dispatch = useDispatch();
-  const [data, setData] = useState();
-
-  useSelector(state => setData(state.crudCharacters.charactersRetrieveReducer));
-  useEffect(() => { dispatch(recuperarPersonagensRequest()); })
-
+  const data = useSelector(state => state.crudCharacters.charactersRetrieveReducer);
+  useEffect(() => { dispatch(recuperarPersonagensRequest()); }, [])
   return (
     <>
-      {JSON.stringify(data)}
+      {JSON.stringify(data.results)}
     </>
   )
 }
